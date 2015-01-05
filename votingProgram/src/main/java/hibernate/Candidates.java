7a52a32fd625d5ed91aec877483a8568fc660ab7
PPIETRUC <PPIETRUC@CE08250.corp.capgemini.com>
2015-01-05 14:55:12 +0100

    hibernate class

--- votingProgram/src/main/java/hibernate/Candidates.java
+++ votingProgram/src/main/java/hibernate/Candidates.java
@@ -6,7 +6,6 @@ import javax.persistence.Table;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
-
 @Entity
 @Table (name = "candidates")
 public class Candidates {
@@ -14,12 +13,37 @@ public class Candidates {
 	@Id
 	@GeneratedValue @Column (name="id")
 	private int id;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
 	
 	@Column(name="firsname")
-	private String firsname;
+	private String firstname;	
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	
 	@Column (name="surname")
 	private String surname;
+	public String getSurname() {
+		return surname;
+	}
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+	
 	@Column (name = "zip_codes_id")
 	private int zip_codes_id;
-	
+	public int getZip_codes_id() {
+		return zip_codes_id;
+	}
+	public void setZip_codes_id(int zip_codes_id) {
+		this.zip_codes_id = zip_codes_id;
+	}
 }
--- votingProgram/src/main/java/hibernate/Elections.java
+++ votingProgram/src/main/java/hibernate/Elections.java
@@ -16,8 +16,26 @@ public class Elections {
 		@Id
 		@GeneratedValue @Column (name="id")
 		private int id;
+		public int getId() {
+			return id;
+		}
+		public void setId(int id) {
+			this.id = id;
+		}
 		@Column (name ="election_date")
 		private Date election_date;
+		public Date getElection_date() {
+			return election_date;
+		}
+		public void setElection_date(Date election_date) {
+			this.election_date = election_date;
+		}
 		@Column (name = "type")
 		private String type;
+		public String getType() {
+			return type;
+		}
+		public void setType(String type) {
+			this.type = type;
+		}
 }
