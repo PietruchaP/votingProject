675c7a6da91ffbcd09b7da39dce22281c231c34d
PPIETRUC <PPIETRUC@CE08250.corp.capgemini.com>
2015-01-08 10:46:33 +0100

    changes of hibernate.cfg.xml possition

--- election/dateBaseCore/src/main/java/hibernate/Tables/Results.java
+++ election/dateBaseCore/src/main/java/hibernate/Tables/Results.java
@@ -5,16 +5,16 @@ import javax.persistence.Entity;
 import javax.persistence.Table;
 
 
-@Entity
-@Table (name = "results")
+//@Entity
+//@Table (name = "results")
 public class Results {
 
-	@Column(name="elections_id")
-	private int elections_id;
-	@Column(name="volters_id")
-	private int volters_id;
-	@Column(name="candidates_id")
-	private int candidates_id;
+//	@Column(name="elections_id")
+//	private int elections_id;
+//	@Column(name="volters_id")
+//	private int volters_id;
+//	@Column(name="candidates_id")
+//	private int candidates_id;
 	
 	
 }
